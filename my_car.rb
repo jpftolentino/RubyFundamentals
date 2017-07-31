86d60936f8d20561e32f661e67d426f6d3429931
John Tolentino <jpftolentino@gmail.com>
2017-07-31 04:59:51 +0000

    Added a few notes that go in detail about OOP

--- my_car.rb
+++ my_car.rb
@@ -100,3 +100,151 @@ porsche.to_s
 # 
 # within a class and outside instance method
 # def self.name=(n) is the same as def GoodDog.name=(n)
+
+# Class Inheritance
+# class Animal
+#   def speak
+#     "Hello!"
+#   end
+# end
+
+# class GoodDog < Animal
+# end
+#
+# Overriding methods inherited from super class
+# class Animal
+#   def speak
+#     "Hello!"
+#   end
+# end
+
+# class GoodDog < Animal
+#   attr_accessor :name
+
+#   def initialize(n)
+#     self.name = n
+#   end
+
+#   def speak
+#     "#{self.name} says arf!"
+#   end
+# end
+
+# SUPER
+# invokes method of the same name from super class
+# must be inside method of object with the same name
+# object must be a subclass of superclass
+# class Animal
+#   def speak
+#     "Hello!"
+#   end
+# end
+#
+# class GoodDog < Animal
+#   def speak
+#     super + " from GoodDog class"
+#   end
+# end
+#
+# class BadDog < Animal
+#   def initialize(age, name)
+#     super(name)
+#     @age = age
+#   end
+# end
+
+# MODULES
+# module Swimmable
+#   def swim
+#     "I'm swimming!"
+#   end
+# end
+
+# class Animal; end
+
+# class Fish < Animal
+#   include Swimmable         # mixing in Swimmable module
+# end
+# sparky = Dog.new
+# neemo  = Fish.new
+# paws   = Cat.new
+
+# sparky.swim                 # => I'm swimming!
+# neemo.swim                  # => I'm swimming!
+# paws.swim                   # => NoMethodError: undefined method `swim' for #<Cat:0x007fc453152308>
+
+# Inheritance vs Modules 
+# Modules are used only for namespacing and grouping common methods together.
+# If it's an "is-a" relationship, choose class inheritance. If it's a "has-a" relationship, choose modules
+
+# Method Lookup Path
+# puts classname.ancestors
+# puts objectname.ancestors
+# last module called is the first in ancestor chain
+
+# More Modules
+# Namespacing
+# module Mammal
+#   class Dog
+#     def speak(sound)
+#       p "#{sound}"
+#     end
+#   end
+
+#   class Cat
+#     def say_name(name)
+#       p "#{name}"
+#     end
+#   end
+# end
+# buddy = Mammal::Dog.new
+# kitty = Mammal::Cat.new
+# buddy.speak('Arf!')           # => "Arf!"
+# kitty.say_name('kitty')       # => "kitty"
+
+# Container
+# module Mammal
+#   ...
+
+#   def self.some_out_of_place_method(num)
+#     num ** 2
+#   end
+# end
+# value = Mammal.some_out_of_place_method(4)
+
+# private methods
+# class GoodDog
+#   DOG_YEARS = 7
+
+#   attr_accessor :name, :age
+
+#   def initialize(n, a)
+#     self.name = n
+#     self.age = a
+#   end
+
+#   private
+
+#   def human_years
+#     age * DOG_YEARS
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky", 4)
+# sparky.human_years
+
+#protected methods
+# class Animal
+#   def a_public_method
+#     "Will this work? " + self.a_protected_method
+#   end
+
+#   protected
+
+#   def a_protected_method
+#     "Yes, I'm protected!"
+#   end
+# end
+
+# fido = Animal.new
+# fido.a_public_method        # => Will this work? Yes, I'm protected!
