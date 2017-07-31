b929e3d9db75f69ae7811cc4aac0f91d67f1de07
John Tolentino <jpftolentino@gmail.com>
2017-07-31 03:48:30 +0000

    learned more content about OOP in Ruby

--- my_car.rb
+++ my_car.rb
@@ -1,9 +1,13 @@
 class MyCar
-    # new instance of object
-    # year, color, model, speed = 0
+    # allows you to get and set color
+    # using objectname.color = 'whatver'
     attr_accessor :color
+
+    # allows you to only use get 
+    # puts objectname.year displays info
     attr_reader :year
 
+    # new instance of object     
     def initialize(y, c, m)
         @year = y
         @color = c
@@ -11,11 +15,21 @@ class MyCar
         @speed = 0
     end
 
+    # add a class method that calculates the gas mileage of any car
+    # can call method withou creating instance of object?
+    def self.gas_mileage
+        puts "#{miles / gallons} miles per gallon of gas"
+    end
+
+    # self. notation only works if it is an attr in the class
     def spray_paint(color)
         self.color = color
+
+        # inside the method so no self.color needed
         puts "Your new #{color} paint job looks great!"
     end
 
+    # not an attr so need to use @ notation
     def speed_up(number)
         @speed += number
     end
@@ -32,6 +46,10 @@ class MyCar
         puts "You are now going #{@speed} mph."
     end
 
+    def to_s
+        puts "This is a #{@color} #{@model} made in #{@year}"
+    end
+
 end
 
 porsche = MyCar.new(2017, 'white', '911')
@@ -44,3 +62,41 @@ porsche.current_speed
 puts porsche.color
 puts porsche.year
 porsche.spray_paint('pink')
+porsche.to_s
+
+# Class Variables
+# keeps track of number of times
+# object is initialized
+# @@number_of_cars = 0
+# def initialize
+#     @@number_of_cars += 1 
+# end
+# def self.total_number_of_cars
+#     @@number_of_cars
+# end
+# puts MyCar.total_number_of_cars
+
+# Constants
+# DOG_YEARS = 7
+# variable will never change
+
+# to_s
+# puts objectname is the same as puts objectname.to_s
+# returns classname and meta id of object
+# you can override the to_s method
+#   def to_s
+#     "This dog's name is #{name} and it is #{age} in dog years."
+#   end
+# puts classname
+
+# Inspect
+# p objectname 
+# Helpful fo debugging purposes
+
+# MORE ABOUT SELF ##### ask about it in lecture!
+# need to be an accessor attr
+# within a class and within instance method
+# self.weight= is the same as sparky.weight=
+# 
+# within a class and outside instance method
+# def self.name=(n) is the same as def GoodDog.name=(n)
